289aee399298345007074e9630c797cd2fadd3ad
Praveena-manis <143096344+Praveena-manis@users.noreply.github.com>
2025-06-09 23:03:57 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,30 @@
-const mongoose=require('mongoose');
+// const mongoose=require('mongoose');
 
-const connectDB=async () => {
-    try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/restapi"||process.env.MONGO_URI);
+// const connectDB=async () => {
+//     try {
+//         await mongoose.connect("mongodb://127.0.0.1:27017/restapi"||process.env.MONGO_URI);
         
-        console.log('Connected');
+//         console.log('Connected');
         
-    } catch (error) {
-        console.log(error);
+//     } catch (error) {
+//         console.log(error);
         
-    }
-}
-module.exports=connectDB;
+//     }
+// }
+// module.exports=connectDB;
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error('DB connection error:', error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
