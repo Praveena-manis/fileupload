25abdcf133c317ec7ed3f6af9a4e942a77aa205b
Praveena-manis <143096344+Praveena-manis@users.noreply.github.com>
2025-06-10 23:57:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express=require('express');
 const dotenv=require('dotenv');
+const cors=require('cors');
 dotenv.config();
 const authController=require('./routes/user.routes');
 const connectDB=require('./config/db');
 const app=express();
 app.use(express.json());
 connectDB();
+app.use(cors({
+  origin: 'https://connect12.netlify.app', // allow only your frontend
+  credentials: true, // if you use cookies or auth headers
+}));
 
 app.use('/api/auth',authController);
 const PORT=process.env.PORT;
